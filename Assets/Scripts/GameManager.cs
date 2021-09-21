00652eb3e3c8cb00c2b25c1a114cc135d4a99666
SovietChan <52289632+SovietChan@users.noreply.github.com>
2021-09-21 10:18:28 +0700

    Save/Load Module

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -41,6 +41,7 @@ public class GameManager : MonoBehaviour
     private void Start ()
     {
         AddAllResources ();
+        GoldInfo.text = $"Gold: { UserDataManager.Progress.Gold.ToString("0") }";
     }
 
     private void Update ()
@@ -61,13 +62,14 @@ public class GameManager : MonoBehaviour
 
     private void AddAllResources ()
     {
+        int index = 0;
         bool showResources = true;
         foreach (ResourceConfig config in ResourcesConfigs)
         {
             GameObject obj = Instantiate (ResourcePrefab.gameObject, ResourcesParent, false);
             ResourceController resource = obj.GetComponent<ResourceController> ();
 
-            resource.SetConfig (config);
+            resource.SetConfig(index, config);
             obj.gameObject.SetActive (showResources);
 
             if (showResources && !resource.IsUnlocked)
@@ -76,6 +78,7 @@ public class GameManager : MonoBehaviour
             }
 
             _activeResources.Add (resource);
+            index++;
         }
     }
 
@@ -98,11 +101,11 @@ public class GameManager : MonoBehaviour
             bool isBuyable = false;
             if (resource.IsUnlocked)
             {
-                isBuyable = TotalGold >= resource.GetUpgradeCost ();
+                isBuyable = UserDataManager.Progress.Gold >= resource.GetUpgradeCost ();
             }
             else
             {
-                isBuyable = TotalGold >= resource.GetUnlockCost ();
+                isBuyable = UserDataManager.Progress.Gold >= resource.GetUnlockCost ();
             }
 
             resource.ResourceImage.sprite = ResourcesSprites[isBuyable ? 1 : 0];
@@ -129,8 +132,9 @@ public class GameManager : MonoBehaviour
 
     public void AddGold (double value)
     {
-        TotalGold += value;
-        GoldInfo.text = $"Gold: { TotalGold.ToString ("0") }";
+        UserDataManager.Progress.Gold += value;
+        GoldInfo.text = $"Gold: { UserDataManager.Progress.Gold.ToString ("0") }";
+        UserDataManager.Save();
     }
 
     public void CollectByTap (Vector3 tapPosition, Transform parent)
